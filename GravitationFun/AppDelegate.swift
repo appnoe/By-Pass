c73f86b2029b1ccfb048f7d7aef3a8b3023e658d
Klaus M. Rodewig <kmr@appnoe.de>
2026-03-14 23:40:53 +0100

    ♻️ Migrate to UIScene lifecycle with SceneDelegate

--- GravitationFun/AppDelegate.swift
+++ GravitationFun/AppDelegate.swift
@@ -1,5 +1,4 @@
 //  Created by Dominik Hauser on 22.12.21.
-//  
 //
 
 import UIKit
@@ -7,17 +6,13 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-  var window: UIWindow?
-
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-
     application.isIdleTimerDisabled = true
-
-    window = UIWindow(frame: UIScreen.main.bounds)
-    window?.rootViewController = GameViewController()
-    window?.makeKeyAndVisible()
-
     return true
+  }
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
   }
 }
 
