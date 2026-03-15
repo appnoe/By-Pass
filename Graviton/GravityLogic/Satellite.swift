830d07387a0890e1060b9790504ed86255413a22
Klaus M. Rodewig <kmr@appnoe.de>
2026-03-15 11:43:33 +0100

    ♻️ Replace force cast with compactMap and remove dead code in Satellite
    - Use compactMap instead of filter+force cast for SKEmitterNode children
    - Remove unused commented-out RectangleEmitter integration block
    - Remove orphaned comment stubs (type property, restitution, print)

--- Graviton/GravityLogic/Satellite.swift
+++ Graviton/GravityLogic/Satellite.swift
@@ -17,7 +17,6 @@ public enum Direction {
 
 public class Satellite: SKShapeNode {
 
-//  let type: SatelliteType
   public let radius: CGFloat = 8
   var colorRatio: CGFloat = 0
   private var planetType: PlanetType = .rocky
@@ -42,7 +41,6 @@ public class Satellite: SKShapeNode {
       let maxValue = sceneSize.width/2 - 10
       var randomX = CGFloat.random(in: 50..<maxValue)
       let randomYVelocity = CGFloat.random(in: 50..<180)
-//      print("(\(randomX), \(randomYVelocity))")
       if clockWise {
         randomX *= -1
       }
@@ -78,9 +76,6 @@ public class Satellite: SKShapeNode {
   }
 
   required init?(coder aDecoder: NSCoder) {
-
-//    type = .box
-
     super.init(coder: aDecoder)
   }
 
@@ -122,7 +117,7 @@ public class Satellite: SKShapeNode {
       spriteNode?.colorBlendFactor = 0.6
     }
     let trailColor = UIColor(hue: colorRatio, saturation: 0.8, brightness: 1.0, alpha: 1)
-    let emitters = children.filter({ $0 is SKEmitterNode }) as! [SKEmitterNode]
+    let emitters = children.compactMap({ $0 as? SKEmitterNode })
     for emitter in emitters {
       emitter.particleColor = trailColor
     }
@@ -131,7 +126,6 @@ public class Satellite: SKShapeNode {
   func addPhysicsBody(with velocity: CGVector) {
     self.physicsBody = SKPhysicsBody(circleOfRadius: radius)
     physicsBody?.friction = 0
-//    physicsBody?.restitution = 0
     physicsBody?.linearDamping = 0
     physicsBody?.angularDamping = 0
     physicsBody?.categoryBitMask = PhysicsCategory.satellite
@@ -158,24 +152,5 @@ public class Satellite: SKShapeNode {
     }
     emitterCopy.particleColor = trailColor
         addChild(emitterCopy)
-
-//      case .rectangle:
-//        guard let emitterCopy = emitterRectangle?.copy() as? SKEmitterNode else {
-//          return
-//        }
-//
-//        emitterCopy.particleColor = fillColor
-//        emitterCopy.position = CGPoint(x: 0, y: -10)
-//        addChild(emitterCopy)
-//
-//        guard let emitterCopy2 = emitterRectangle?.copy() as? SKEmitterNode else {
-//          return
-//        }
-//
-//        emitterCopy2.particleColor = fillColor
-//        addChild(emitterCopy2)
-//
-//        emitterCopy2.position = CGPoint(x: 0, y: 10)
-//    }
   }
 }
